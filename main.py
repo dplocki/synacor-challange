755bffd78e0b9fd36af0fa35bca4cbb556af93f1
Dawid Plocki <dawid.plocki@gmail.com>
2021-01-02 16:48:13 +0100

    Adding script parameteres

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
+import sys
+
 from vm.utils import load_program
 from vm.virtual_machine import VirtualMachine
 from vm.standard_io import StandardIO
 from vm.debug_virtual_machine import DebugVirtualMachine
 
 
-vm = VirtualMachine(StandardIO())
-#vm = DebugVirtualMachine()
-
+vm = DebugVirtualMachine() if len(sys.argv) > 1 and sys.argv[1] == '-d' else VirtualMachine(StandardIO())
 vm.load_program(load_program('challenge.bin'))
 vm.run()
