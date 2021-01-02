30cced78d91fefbfd227d8b158ab60461f4bc69d
Dawid Plocki <dawid.plocki@gmail.com>
2021-01-02 23:11:50 +0100

    Implement input reading

--- vm/standard_io.py
+++ vm/standard_io.py
@@ -1,7 +1,15 @@
 class StandardIO():
 
+    def __init__(self):
+        self.buffor = None
+
     def write(self, what):
         print(chr(what), end='')
 
     def read(self):
-        return input()
+        if not self.buffor:
+            self.buffor = list(input())
+            self.buffor.append('\n')
+            self.buffor.reverse()
+
+        return ord(self.buffor.pop())
