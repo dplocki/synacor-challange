a565fafd75fa94dc608ce44b41e41c27cdab9cbb
Dawid Plocki <dawid.plocki@gmail.com>
2021-01-04 20:47:53 +0100

    Reorganize decompiler feature

--- decompile.py
+++ decompile.py
@@ -1,6 +1,17 @@
 from sys import argv
 from vm.utils import load_dump_file
-from vm.decompiler import generate_listing
+from vm.decompiler import read_program
+from vm.debug_virtual_machine import parameter_to_string, format_parameter
+
+
+
+def out_param(value: int) -> str:
+    if value == 10:
+        return '\\n'
+    elif value <= 255:
+        return chr(value)
+
+    return parameter_to_string(value)
 
 
 LOAD_DUMP_FLAG = '-l'
@@ -12,4 +23,21 @@ else:
     dump_file_name = args[args.index(LOAD_DUMP_FLAG) + 1]
     dump = load_dump_file(dump_file_name)
 
-    generate_listing(dump['memory'])
+    decompiled_program = read_program(dump['memory'])
+
+    call_indexes = set(paramaters[0] for _, name, paramaters in decompiled_program if name == 'call')
+
+    for index, name, paramaters in read_program(dump['memory']):
+        print('*' if index in call_indexes else ' ', end='')
+        print(f'{index:>7}: ', end='')
+        if name:
+            print(f'{name:>7}  ', end='')
+            if name == 'out':
+                print(out_param(paramaters[0]), end='')
+            elif name == 'ret':
+                print()
+            elif paramaters:
+                print(*map(format_parameter, map(parameter_to_string, paramaters)), end='')
+        else:
+            print(paramaters, end='')
+        print()
--- vm/decompiler.py
+++ vm/decompiler.py
@@ -1,33 +1,15 @@
-from vm.debug_virtual_machine import OPTCODES_WITH_PARAMETERS, parameter_to_string
+from vm.debug_virtual_machine import OPTCODES_WITH_PARAMETERS
 
 
-def generate_listing(program: [int]) -> None:
-
-    def out_param(value: int) -> str:
-        if value == 10:
-            return '\\n'
-        elif value <= 255:
-            return chr(value)
-
-        return parameter_to_string(value)
-
-
+def read_program(program: [int]) -> [tuple]:
     index, index_max = 0, len(program)
-
     while index < index_max:
-        print(f'{index:>7}: ', end='')
-
-        if program[index] in OPTCODES_WITH_PARAMETERS:
-            name, parameters_number = OPTCODES_WITH_PARAMETERS[program[index]]
-
-            print(f'{name:>7}', f'{" ".join(map(parameter_to_string, program[index + 1: index + 1 + parameters_number])):40}', end='')
-
-            if name == 'out':
-                print('|', out_param(program[index + 1]), end='')
-
-            print()
-            index += parameters_number
+        current = program[index]
+        if current not in OPTCODES_WITH_PARAMETERS:
+            yield index, None, current
         else:
-            print(index)
+            name, parameters_number = OPTCODES_WITH_PARAMETERS[current]
+            yield index, name, tuple(program[index + 1: index + 1 + parameters_number])
+            index += parameters_number
 
         index += 1
