82c1ed80a953b7d87eab7d37936de66c0a291e62
Dawid Plocki <dawid.plocki@gmail.com>
2021-01-08 22:21:04 +0100

    Unification of puzzle resposnes

--- puzzles/door_puzzle.py
+++
@@ -1,6 +0,0 @@
-from itertools import permutations
-
-digits = [2, 7, 9, 3, 5]
-permutations = ((a, b, c, d, e) for a, b, c, d, e in permutations(digits, 5) if a + b * c ** 2 + d ** 3 - e == 399)
-a, b, c, d, e = next(permutations)
-print(f'{a} + {b} * {c}^2 + {d}^3 - {e} = 399')
